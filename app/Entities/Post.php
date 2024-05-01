a9ce1abcfa877befccea73f4fca0c8f591dda349
ezlo-mukhtarove <emilmuhtarov1@gmail.com>
2024-05-01 11:42:37 +0200

    Response redirect

---
+++ app/Entities/Post.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entities;
+
+class Post
+{
+    public function __construct(
+        private ?int $id,
+        private string $title,
+        private string $body,
+        private \DateTimeImmutable|null $createdAt
+    )
+    {
+    }
+
+    public static function create(
+         $title, $body, $id = null, $createdAt = null
+    ): static
+    {
+        return new static($id, $title, $body, $createdAt ?? new \DateTimeImmutable());
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @param string $body
+     */
+    public function setBody(string $body): void
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @param \DateTimeImmutable|null $createdAt
+     */
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+}
